85aa999ca43bd20ba31cb8a9f0b1045fdd45b6b2
BARANCEANU Vlad-Andrei <vladandreibaranceanu@gmail.com>
2025-01-22 11:52:54 +0200

    Added basic staff and can recognize user

--- Types/PCAP/src/PayloadParsers/FTPParser.cpp
+++ Types/PCAP/src/PayloadParsers/FTPParser.cpp
@@ -10,8 +10,33 @@ PayloadDataParserInterface* FTP::FTPParser::ParsePayload(
     if (payloadInformation.payload->size < 10)
         return nullptr;
 
-    if (memcmp(payloadInformation.payload->location, "220", 3) != 0)
+    auto& var = payloadInformation.payload->location;
+    if (memcmp(payloadInformation.payload->location, "220", 3) != 0 && memcmp(payloadInformation.payload->location, "120", 3) != 0 &&
+              memcmp(payloadInformation.payload->location,
+        "421", 3) != 0 && memcmp(payloadInformation.payload->location, "221", 3) != 0)
         return nullptr;
+
+    uint32 src = 0;
+    for (auto& packet : *payloadInformation.packets) {
+        if (packet.packetData.linkLayer.has_value()) {
+            if (packet.packetData.linkLayer->type == LinkType::IPV4) {
+                auto ipv4 = (IPv4Header*) packet.packetData.linkLayer->header;
+            }
+        }
+        if (packet.packetData.transportLayer.has_value()) {
+            if (packet.packetData.transportLayer->transportLayer == IP_Protocol::TCP) {
+                auto tcp = (TCPHeader*) packet.packetData.transportLayer->transportLayerHeader;
+            }
+        }
+
+        if (packet.payload.size > 0) //current payload has payload
+        {
+            //..
+            packet.payload.location;
+        }
+    }
+
+    
 
     callbackInterface->AddConnectionSummary("FTP initial connection");
     callbackInterface->AddConnectionAppLayerName("FTP");
@@ -35,5 +60,44 @@ PayloadDataParserInterface* FTP::FTPParser::ParsePayload(
     memcpy(layer2.payload.location, data, layer2.payload.size + 1);
 
     applicationLayers.emplace_back(std::move(layer2));
+
+
+    const uint8* startPtr = connPayload->location;
+    const uint8* startline = connPayload->location;
+    const uint8* endPtr   = connPayload->location + connPayload->size;
+    
+    bool command = 0;
+    while (startPtr < endPtr) {
+        if (*startPtr == 0x0D || *startPtr == 0x0a) {
+            
+            if (startPtr - startline > 0) {
+                unsigned char* sequence = new unsigned char[startPtr - startline];
+                std::memcpy(sequence, startline, startPtr - startline);
+                sequence[startPtr - startline + 1] = '\0';
+                startline                          = startPtr + 2;
+                if (memcmp(sequence, "USER", 4) == 0) {
+                    StreamTcpLayer layer = {};
+                    const char* name     = "Username: ";
+                    unsigned char* onlyName = sequence + 5;
+
+                    size_t nameLength         = std::strlen(name);
+                    size_t modifiedDataLength = std::strlen(reinterpret_cast<const char*>(onlyName));
+
+                    size_t totalLength = nameLength + modifiedDataLength + 1;
+                    char* concatenated = new char[totalLength];
+                    std::memcpy(concatenated, name, nameLength);
+                    std::memcpy(concatenated + nameLength, onlyName, modifiedDataLength);
+                    concatenated[totalLength - 1] = '\0';
+
+                    layer.name = std::make_unique<uint8[]>(strlen(concatenated) + 1);
+                    memcpy(layer.name.get(), concatenated, strlen(concatenated) + 1);
+                    applicationLayers.emplace_back(std::move(layer));
+                }
+
+            }
+        
+        }
+        startPtr++;
+    }
     return this;
 }
--- Types/PCAP/src/PayloadParsers/FTPParser.hpp
+++ Types/PCAP/src/PayloadParsers/FTPParser.hpp
@@ -11,6 +11,45 @@ struct FTPParser : public PayloadDataParserInterface {
         return "FTP";
     }
 
+    std::unordered_map<std::string, std::string> ftp_transfer_parameter_commands = {
+        { "PORT", "DATA PORT" }, { "PASV", "PASSIVE" }, { "TYPE", "REPRESENTATION TYPE" }, { "STRU", "FILE STRUCTURE" }, { "MODE", "TRANSFER MODE" },
+    };
+
+    std::unordered_map<std::string, std::string> ftp_access_control_commands = {
+        { "USER", "USER NAME" },
+        { "PASS", "PASSWORD" },
+        { "ACCT", "ACCOUNT" },
+        { "CWD", "CHANGE WORKING DIRECTORY" },
+        { "CDUP", "CHANGE TO PARENT DIRECTORY" },
+        { "SMNT", "STRUCTURE MOUNT " },
+        { "REIN", "REINITIALIZE" },
+        { "QUIT", "LOGOUT " },
+    };
+
+    std::unordered_map<std::string, std::string> ftp_service_commands = {
+        { "RETR", "RETRIEVE" },
+        { "STOR", "STORE" },
+        { "STOU", "STORE UNIQUE" },
+        { "APPE", "APPEND (with create)" },
+        { "ALLO", "ALLOCATE" },
+        { "REST", "RESTART " },
+        { "RNFR", "RENAME FROM" },
+        { "RNTO", "RENAME TO" },
+        { "ABOR", "ABORT" },
+        { "DELE", "DELETE" },
+        { "RMD", "REMOVE DIRECTORY" },
+        { "MKD", "MAKE DIRECTORY" },
+        { "PWD", "PRINT WORKING DIRECTORY" },
+        { "LIST", "LIST" },
+        { "NLST", "NAME LIST" },
+        { "SITE", "SITE PARAMETERS" },
+        { "SYST", "SYSYEM" },
+        { "STAT", "STATUS" },
+        { "HELP", "HELP" },
+        { "NOOP", "NOOP" },
+
+    };
+
     PayloadDataParserInterface* ParsePayload(const PayloadInformation& payloadInformation, ConnectionCallbackInterface* callbackInterface) override;
 };
 }
